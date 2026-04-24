49cb22de9b3f8aa33181766a33a45072b9d63526
João Pedro <Joao.Pedro.MDP@outlook.com>
2026-04-24 20:47:29 -0300

    fix: Apenas restart não reiniciava o container com a nova imagem

--- loadimage.py
+++ loadimage.py
@@ -6,11 +6,13 @@ def main(back: bool, front: bool):
     # executa docker compose build
     if back:
         os.system("docker load < /images/back.tar.gz")
-        os.system("docker compose restart back")
+        os.system("docker compose down back")
+        os.system("docker compose up -d back")
 
     if front:
         os.system("docker load < /images/front.tar.gz")
-        os.system("docker compose restart front")
+        os.system("docker compose down front")
+        os.system("docker compose up -d front")
 
 if __name__ == "__main__":
     back = False
