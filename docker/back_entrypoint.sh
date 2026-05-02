b04ed346776472d407c9a198eda78a41fceb4a68
João Pedro <Joao.Pedro.MDP@outlook.com>
2026-05-02 16:56:32 -0300

    change: entrypoint chama python e daphne com caminhos absolutos

--- docker/back_entrypoint.sh
+++ docker/back_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python manage.py collectstatic --noinput
-python manage.py migrate
-daphne -b 0.0.0.0 -p 8000 core.asgi:application
+/app/.venv/bin/python manage.py collectstatic --noinput
+/app/.venv/bin/python manage.py migrate
+/app/.venv/bin/daphne -b 0.0.0.0 -p 8000 core.asgi:application
