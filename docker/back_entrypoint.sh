02f6fdd8afe12b6c7048cc35779a9b5ac4050c9a
João Pedro <Joao.Pedro.MDP@outlook.com>
2026-05-02 17:09:13 -0300

    change: Revertendo alterações no entrypoint

--- docker/back_entrypoint.sh
+++ docker/back_entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/app/.venv/bin/python manage.py collectstatic --noinput
-/app/.venv/bin/python manage.py migrate
-/app/.venv/bin/daphne -b 0.0.0.0 -p 8000 core.asgi:application
+python manage.py collectstatic --noinput
+python manage.py migrate
+daphne -b 0.0.0.0 -p 8000 core.asgi:application
